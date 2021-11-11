6b9ffc3c69c8d86acec0ec297ad2c4d7f04e0e3a
AlexZh777 <a.zharaspaev@hotmail.com>
2021-11-12 01:14:18 +0300

    Revised schedule_parse()

--- parser/__main__.py
+++ parser/__main__.py
@@ -47,47 +47,31 @@ def create_database():
 	print('Rows successfully inserted.')
 
 def scheduled_parse():
+	print('Current time is {}'.format(str(datetime.now())))
 	page = parser.get_page()
 	all_posts = parser.parse_front_page_full(page)
 
-	latest_post_time = parser.get_post_time(all_posts[0])
-	print('As of {}, the most recent news was posted @ {}'.format(str(datetime.now()), latest_post_time))
-	print('Checking if a post with this time exists in the database...')
+	for i in range(len(all_posts)):
+		latest_post_time = parser.get_post_time(all_posts[i])
+		print('Recent news was posted @ {}'.format(latest_post_time))
+		print('Checking if a post with this time exists in the database...')
 
-	engine = create_engine('sqlite:///' + get_db_filepath())
-	
-	with Session(engine) as session:
-		count = session.query(NewsPost).filter(NewsPost.time_posted == latest_post_time).count()
-	
-	if count == 1:
-		print('This post already exists in the database. Will check again in {} minutes.'.format(SCHEDULER_CONFIG))
-	else:
-		print('A new post has appeared! Capturing into the database...')
-		latest_post = all_posts[0]
-		with Session(engine) as session:
-			row = NewsPost(**latest_post)
-			session.add(row)
-			session.commit()
-		print('Successfully saved the post. Now checking if the next post already exists...')
-		# Я понимаю, что я здесь нарушаю DRY, да и в принципе это точно можно сделать лучше.
-		# И это не будет работать, если три+ новости в течение 10 минут
-		# Но у меня уже голова не варит, и я видел только один edge case, где две новости в течение 10 минут.
-		# Если успею, утром пересмотрю.
+		engine = create_engine('sqlite:///' + get_db_filepath())
 
-		latest_post_time = parser.get_post_time(all_posts[1])
 		with Session(engine) as session:
 			count = session.query(NewsPost).filter(NewsPost.time_posted == latest_post_time).count()
 
 		if count == 1:
-			print('The next post already exists in the database. Will check again in 10 minutes.')
+			print('This post already exists in the database. Will check again in {} minutes.'.format(SCHEDULER_CONFIG))
+			break
 		else:
-			print('Edge case: two posts within 10 minutes! Capturing second post in the database...')
-			latest_post = all_posts[1]
+			print('New post detected! Capturing into the database...')
+			latest_post = all_posts[i]
 			with Session(engine) as session:
 				row = NewsPost(**latest_post)
 				session.add(row)
 				session.commit()
-
+			print('Successfully saved the post. Checking if the next post already exists...')
 
 if __name__ == '__main__':
 	parser = Parser()
@@ -96,7 +80,7 @@ if __name__ == '__main__':
 
 	if not os.path.isfile(db_filepath):
 		create_database()
-	
+
 	print('The database exists! Launching parser, scheduled for every {} minutes...'.format(str(SCHEDULER_CONFIG)))
 	schedule.every(SCHEDULER_CONFIG).minutes.do(scheduled_parse)
 
